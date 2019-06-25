262e66e61e23bef1aefb81b8874aa0df8694dd69
kristyyang1110 <kristyyang1110@gmail.com>
2019-06-25 16:02:55 -0700

    create newMovie

--- movie/src/components/Content.js
+++ movie/src/components/Content.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import NewMovie from './NewMovie';
 
 const ContentPadding = styled.p`
     padding-top: 0.6rem;
@@ -19,7 +20,7 @@ function Content() {
                 {
                     title: movie["title"],
                     overview: movie["overview"],
-                    poster: movie["poster_path"],
+                    poster: `https://image.tmdb.org/t/p/w500${movie["poster_path"]}`,
                     voteAvg: movie["vote_average"],
                     releaseDate: movie["release_date"]
                 }
@@ -30,45 +31,46 @@ function Content() {
     }, [])
     return (
         <div className="section">
-            <div className="container has-text-centered">
-                <ul>
-                    {movies.map((movie, idx) => <li key={idx}>{movie.title}</li>)}
-                </ul>
-                {/* <p className="subtitle">
-                    Find your favorite movies here <span role="img" aria-label="kiss">😘</span>
-                </p>
-                <div className="field">
-                    <div className="control">
-                        <input className="input is-rounded" type="text" placeholder="Movie Name" />
+            <section className="hero is-fullheight has-background-light">
+                <div className="hero-body">
+                    <div className="columns is-multiline">
+                        {movies && movies.map((movie, idx) => (
+                            <div className="column is-12-tablet is-6-desktop is-4-widescreen">
+                                <NewMovie
+                                    key={idx}
+                                    image={movie.poster}
+                                    title={movie.title}
+                                    description={movie.overview}
+                                    rate={movie.voteAvg}
+                                    date={movie.releaseDate} />
+                            </div>
+                        ))}
                     </div>
                 </div>
-                <div className="level">
-                    <div className="level-left">
-                        <div className="level-item">
-                            <button className="button is-primary is-rounded is-focused has-text-weight-semibold has-icons-left">Submit</button>
+            </section>
+            <div className="container has-text-centered">
+                <ContentPadding>
+                    <p className="subtitle">
+                        Find your favorite movies here <span role="img" aria-label="kiss">😘</span>
+                    </p>
+                    <div className="field">
+                        <div className="control">
+                            <input className="input is-rounded" type="text" placeholder="Movie Name" />
                         </div>
                     </div>
-                    <div className="level-right">
-                        <div className="level-item">
-                            <a className="button is-link is-rounded  is-focused has-text-weight-semibold" href="https://www.themoviedb.org/">I want to have good time</a>
-                        </div>
-                    </div>
-                </div> */}
-                {/* <div className="level">
-                    <div className="control">
-                        <div className="level-item">
-                            <ContentPadding>
+                    <div className="level">
+                        <div className="level-left">
+                            <div className="level-item">
                                 <button className="button is-primary is-rounded is-focused has-text-weight-semibold has-icons-left">Submit</button>
-                            </ContentPadding>
-                        </div> */}
-
-                {/* <div className="buttons is-right">
-                    <div className="level-item">
-                        <ContentPadding>
-                            <a className="button is-link is-rounded  is-focused has-text-weight-semibold" href="https://www.themoviedb.org/">I want to have good time</a>
-                        </ContentPadding>
+                            </div>
+                        </div>
+                        <div className="level-right">
+                            <div className="level-item">
+                                <a className="button is-link is-rounded  is-focused has-text-weight-semibold" href="https://www.themoviedb.org/">I want to have good time</a>
+                            </div>
+                        </div>
                     </div>
-                </div> */}
+                </ContentPadding>
             </div>
         </div>
     )
---
+++ movie/src/components/NewMovie.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.a`
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const Image = styled.img`
+    height: 300px;
+    width: 400px;
+`
+
+function NewMovie({ key, image, title, description, rate, date }) {
+    return (
+        <article className="box">
+            <div className="media">
+                <aside className="media-left">
+                    <img src={image} className="image" width="150" />
+                </aside>
+                <div className="media-content">
+                    <Title className="title is-5 is-spaced is marginless ">
+                        {title}
+                    </Title>
+
+                    <div className="content is-small">{description}</div>
+                </div>
+                <div className="level">
+                    <div className="level-right">
+                        <div className="level-item">
+                            <h1>{rate}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    );
+}
+
+
+
+export default NewMovie;
